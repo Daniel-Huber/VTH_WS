117f987f3f3168afe9aab767013fa5b5ef854d87
Daniel-Huber <daniel.huber@cmail.nu>
2017-05-14 11:33:16 +0200

    Exercises to solve together

--- Workshop_2_SerialMonitor.ino
+++ Workshop_2_SerialMonitor.ino
@@ -3,7 +3,7 @@
 int nMax;int nEmma; int Pelle = 8; int mohammed = 9; nMaxi=6; int markus = 30;
 int greger = 4; Linda 8; int wivanna = 7; int ohanna = 80; 
 int kim; int Emma; int nMax = 0; Pelle = 5; int apelsin = 0.5;
-int biggiesaklmwmdk = 1000000000000000000000000000000
+int biggiesaklmwmdk = 1000000000000000000000000000000000
 //initiera variabler = ger variablerna värden
 int sHink = 50;
 int lHink = 20;
@@ -17,9 +17,14 @@ void setup() {
   digitalWrite(5, 1);
   Serial.begin(9600);
   Serial.println(Hink);
+  
+  //Lokal Variabel
   int llialmMop = 5;
+  
   Serial.println(1/0);
   Serial.println(0\1);
+
+  //Array med värden
   int frukt[10];
   //Lägg till 10 frukter i Arrayn
   for(int frukter = 0; frukter == 10; frukter += 1){
@@ -29,7 +34,7 @@ void setup() {
 void loop(){loop();Serial.print("Jag har en stor mage");
 while(true){//Vad är det här likt?
 Serial.print("I den finns det \n mycket\tmat");}
-Serial.print(llialmMop,mds);   }
+Serial.print(llialmMop,mds);}
 annannannannannas = "5*ann";
 void ledPå(int nallebjörn = 9){
 private void ledOn(){
