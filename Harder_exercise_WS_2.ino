f5759965ff765360ebd65e48b74da970223b2835
Daniel-Huber <daniel.huber@cmail.nu>
2017-05-14 11:24:27 +0200

    Arduino Serial monitor exercises

---
+++ Harder_exercise_WS_2.ino
@@ -0,0 +1,24 @@
+/*
+ * 5 knappar med input ska installeras och definieras på en kopplingsplatta.
+ * Knapparna ska ha följande funktioner:
+ * Knapp 1 generar slumpmässiga ental
+ * Knapp 2 generar slumpmässiga tiotal
+ * Knapp 3 generar slumpmässiga hundratal
+ * Knapp 4 byter mellan två utskriftslägen.
+ *    Ena läget - Om man trycker på printknappen så skrivs alla tal från vektorn ut i monitorn.
+ *    Andra läget - För varje knapptryckning av 1, 2, 3 så skrivs en sorterad Array ut.
+ * Knapp 5 Printknappen.
+ * Knapp 6 tömmer Arrayn.
+ * 
+ * Tips: Gör funktioner som hjälper till i loopen så att inte loopen blir så klöddig.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ WS_2_exercises.ino
@@ -0,0 +1,67 @@
+int globVar;
+int globVar2;
+
+int skogen = 8;
+int lokomotiv = 90;
+
+void setup() {
+  int 50;
+  pinMode(LED_BUILTIN, OUTPUT);     
+  Serial.begin(9600);
+  Serial.print(LED_BUILTIN);
+  int monno = 9;
+  const int lokVar;
+  monno = 8 + monno * lokomotiv + skogen;
+}
+
+void loop() {
+  serial.print("monno: ");
+  Serial.println(monno);
+  toggle();
+  serial.print(lokVar);
+  delay(1000)                     
+  toggle();  
+  serial.print(lokVar + kiwi);
+  delay(2000);
+  Serial.print(LED_BUILTIN);                     
+}
+
+kiwi = 10;
+
+void toggle(){
+  Serial.print("not digitalRead(): ");
+  Serial.println(!digitalRead(LED_BUILTIN));
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void lampaPÅ(double pin){
+  pinMode(5, OUTPUT);
+  digitalWrite(pin, OUTPUT); 
+}
+
+void pinModeO(int till, int från){
+  int SIST = från;
+  while(1 + till > sist){
+    pinMode(från, INPUT);
+    sist -= 1
+  }
+}
+
+void kjansdms(int dwqUrsm, double aNsmdwqJk = 4, double PstnAldsoaPmmfoI){
+  int något;
+  double någotAnnat
+  int rekt78fma192mszlm[PstnAldsoaPmmfoI];
+  for(något = 9; dwqUrsm <= PstnAldsoaPmmfoI; dwqUrsm -=){
+    rekt78fma192mszlm[något] = dwqUrsm
+  }
+  while(!false){
+    Serial.print("Wat!?");
+  }
+  if(HIGH == 1){
+    for(något = 0; något > någotAnnat; något += 5){
+      Serial.print(rekt78fma192mszlm[något]);
+    }
+  }
+}
+
+
